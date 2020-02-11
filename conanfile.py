11ab777034ffb5c5fd14fddab781066686e1cfdc
shaheen <shaheen@mail.uni-paderborn.de>
2020-02-11 17:02:39 +0100

    s

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class sparqlParserBase(ConanFile):
     requires = ()
     generators = "cmake", "cmake_find_package", "cmake_paths"
     exports = "LICENSE.txt"
-    exports_sources ="ANTL_bin/*","ANTLR_runtime/*", "sparqlGrammer/*", "CMakeLists.txt", "cmake/*"
+    exports_sources ="ANTLR_bin/*","ANTLR_runtime/*", "sparql_Grammer/*", "CMakeLists.txt", "cmake/*"
     no_copy_source = True
 
     def package(self):
@@ -23,9 +23,7 @@ class sparqlParserBase(ConanFile):
         cmake.configure()
         cmake.install()
 
-    def package_id(self):
-        pass
-        # self.info.header_only()
+
 
     def imports(self):
         self.copy("license*", dst="licenses", folder=True, ignore_case=True)

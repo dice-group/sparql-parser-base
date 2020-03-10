802fe7d83538237757de297074871610714ffdb7
shaheen <shaheen@mail.uni-paderborn.de>
2020-03-10 10:56:53 +0100

    make it work

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,17 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake,tools
 
 
 class sparqlParserBase(ConanFile):
-    name = "sparql-parser"
+    name = "sparql-parser-base"
     version = "0.1.0"
     author = "DICE Group <info@dice-research.org>"
     description = "A ANTLR4 base for SPARQL parser."
     homepage = "https://github.com/dice-group/sparql-parser"
     url = homepage
     license = "AGPL"
-    topics = "SPARQL", "parser", "semantic web"
-    settings = "build_type", "compiler", "os", "arch"
-    requires = ()
-    generators = "cmake", "cmake_find_package", "cmake_paths"
+    topics = "SPARQL", "parser", "semantic web","antlr4"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
     exports = "LICENSE.txt"
     exports_sources = (
         "CMakeLists.txt",
@@ -25,7 +24,10 @@ class sparqlParserBase(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.install()
-
+        self.copy("*.a", dst="lib", keep_path=False)
 
     def imports(self):
         self.copy("license*", dst="licenses", folder=True, ignore_case=True)
+
+    def package_info(self):
+        self.cpp_info.libs = ["sparql-parser-base","antlr4-runtime"]

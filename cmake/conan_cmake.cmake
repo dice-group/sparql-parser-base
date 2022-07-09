697393cda1733fe3c6bf0a85e331417a293a7628
Alexander Bigerl <alexander@bigerl.eu>
2022-07-09 15:01:44 +0200

    modernized conan setup for sparql-parser-base

---
+++ cmake/conan_cmake.cmake
@@ -0,0 +1,31 @@
+macro(install_packages_via_conan conanfile conan_options)
+
+    list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
+    list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+
+    if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+        message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake"
+                TLS_VERIFY ON)
+    endif ()
+    include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+    if (BUILD_SHARED_LIBS)
+        SET(conan_options_arg "*:shared=True ${conan_options}")
+        message("in")
+    endif ()
+
+    conan_cmake_autodetect(settings)
+
+    if (IS_TOP_LEVEL AND BUILD_TESTING)
+        set(CONAN_HYPERTRIE_WITH_TEST_DEPS "True")
+    else ()
+        set(CONAN_HYPERTRIE_WITH_TEST_DEPS "False")
+    endif ()
+    conan_cmake_install(PATH_OR_REFERENCE ${conanfile}
+            BUILD missing
+            SETTINGS ${settings}
+            OPTIONS ${conan_options_arg}
+            GENERATOR "CMakeDeps")
+endmacro()
